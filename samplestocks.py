173f0cf9994a4e67c5b3f04ed1023e7b46bea686
Chethan Rao <chethanrao@Chethans-MacBook-Pro.local>
2017-02-10 21:44:10 -0500

    program to pull stocks
    sample program to pull stocks from yahoo api

---
+++ samplestocks.py
@@ -0,0 +1,16 @@
+import urllib2
+import time
+
+stockToPull='GOOGL'
+
+def pullData(stock):
+    try:
+	file=stock+".txt"
+	url='http://chartapi.finance.yahoo.com/instrument/1.0/'+stockToPull+'/chartdata;type=quote;range=1d/csv'
+	data=urllib2.urlopen(url).read()
+	print data
+    except Exception,e:
+	print 'main loop',str(e)
+
+
+pullData(stockToPull)
