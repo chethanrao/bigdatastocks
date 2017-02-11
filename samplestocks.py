8a17bbcd048702ec132c8127792faea170438d13
Chethan Rao <chethanrao@Chethans-MacBook-Pro.local>
2017-02-11 08:47:39 -0500

    print only relevant  stock data

--- samplestocks.py
+++ samplestocks.py
@@ -1,15 +1,28 @@
 import urllib2
-import time
 
 stockToPull='GOOGL'
 
 def pullData(stock):
-    try:
-	url='http://chartapi.finance.yahoo.com/instrument/1.0/'+stockToPull+'/chartdata;type=quote;range=1d/csv'
+	url='http://chartapi.finance.yahoo.com/instrument/1.0/'+stock+'/chartdata;type=quote;range=1d/csv'
 	data=urllib2.urlopen(url).read()
-	print data
-    except Exception,e:
-	print 'main loop',str(e)
+	lines=data.split("\n")
+	for line in lines:
+		firstString=line.split(",")[0]
+		#print firstString
+		if isfloat(firstString) or isint(firstString):
+			print line
 
+def isfloat(value):
+  try:
+    float(value)
+    return True
+  except ValueError:
+    return False
 
+def isint(value):
+  try:
+	int(value)
+	return True
+  except ValueError:
+	return False
 pullData(stockToPull)
